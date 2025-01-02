48a7659f13e1106f14942e905903378286f6546a
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2025-01-02 12:58:59 +0000

    - code cleanup

--- OpenSURF/IPoint.cs
+++ OpenSURF/IPoint.cs
@@ -1,54 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿namespace OpenSURFcs;
 
-namespace OpenSURFcs
+public class IPoint
 {
-  public class IPoint
+  /// <summary>
+  /// Default ctor
+  /// </summary>
+  public IPoint()
   {
-    /// <summary>
-    /// Default ctor
-    /// </summary>
-    public IPoint()
-    {
-      orientation = 0;
-    }
+    orientation = 0;
+  }
 
-    /// <summary>
-    /// Coordinates of the detected interest point
-    /// </summary>
-    public float x, y;
+  /// <summary>
+  /// Coordinates of the detected interest point
+  /// </summary>
+  public float x, y;
 
-    /// <summary>
-    /// Detected scale
-    /// </summary>
-    public float scale;
+  /// <summary>
+  /// Detected scale
+  /// </summary>
+  public float scale;
 
-    /// <summary>
-    /// Response of the detected feature (strength)
-    /// </summary>
-    public float response;
+  /// <summary>
+  /// Response of the detected feature (strength)
+  /// </summary>
+  public float response;
 
-    /// <summary>
-    /// Orientation measured anti-clockwise from +ve x-axis
-    /// </summary>
-    public float orientation;
+  /// <summary>
+  /// Orientation measured anti-clockwise from +ve x-axis
+  /// </summary>
+  public float orientation;
 
-    /// <summary>
-    /// Sign of laplacian for fast matching purposes
-    /// </summary>
-    public int laplacian;
+  /// <summary>
+  /// Sign of laplacian for fast matching purposes
+  /// </summary>
+  public int laplacian;
 
-    /// <summary>
-    /// Descriptor vector
-    /// </summary>
-    public int descriptorLength;
-    public float [] descriptor = null;
-    public void SetDescriptorLength(int Size)
-    {
-      descriptorLength = Size;
-      descriptor = new float[Size];
-    }
+  /// <summary>
+  /// Descriptor vector
+  /// </summary>
+  public int descriptorLength;
+  public float [] descriptor = null;
+  public void SetDescriptorLength(int Size)
+  {
+    descriptorLength = Size;
+    descriptor = new float[Size];
   }
 }
